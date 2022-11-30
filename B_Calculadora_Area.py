f8751f8f23873880e2e5ad09a9f8d7240640d5f7
Andrea Moreno <77305666+andrea-m-t@users.noreply.github.com>
2022-11-30 17:18:26 -0300

    Update B_Calculadora_Area.py

--- B_Calculadora_Area.py
+++ B_Calculadora_Area.py
@@ -1,4 +1,3 @@
-
 #CALCULADORA DE AREA SEGUN LA FORMA
 import math
 
@@ -12,34 +11,38 @@ entrada=(input()).capitalize()
 
 #Proceso de forma
 def proceso (entrada):
-    if entrada == ("A"):
-        print("Ingrese el valor de la Base.")
-        base=float(input())
-        print("Ingrese el valor de la Altura.")
-        altura=float(input())
-        calculo=((base*altura)/2)
-        print ("El área es " + str(calculo) +".")
-    
-    if entrada == ("B" or "C"):
-        print("Ingrese el valor de la Base.")
-        base=float(input())
-        print("Ingrese el valor de la Altura.")
-        altura=float(input())
-        calculo=base*altura
-        print ("El área es " + str(calculo) +".")
-    
-    if entrada == ("D"):
-        print("Ingrese el valor del 1er Diámetro.")
-        d_1=float(input())
-        print("Ingrese el valor del 2do Diámetro.")
-        d_2=float(input())
-        calculo=((d_1*d_2)/2)
-        print ("El área es " + str(calculo) +".")
-    
-    if entrada == ("E"):
-        print("Ingrese el valor del Radio")
-        radio=float(input())
-        calculo=math.pi*(radio)**2
-        print ("El área es " + str(calculo) +".")
+
+    if True:
+            if entrada == ("A"):
+                print("Ingrese el valor de la Base.")
+                base=float(input())
+                print("Ingrese el valor de la Altura.")
+                altura=float(input())
+                calculo=((base*altura)/2)
+                print ("El área es " + str(calculo) +".")
+            
+            if entrada == ("B" or "C"):
+                print("Ingrese el valor de la Base.")
+                base=float(input())
+                print("Ingrese el valor de la Altura.")
+                altura=float(input())
+                calculo=base*altura
+                print ("El área es " + str(calculo) +".")
+            
+            if entrada == ("D"):
+                print("Ingrese el valor del 1er Diámetro.")
+                d_1=float(input())
+                print("Ingrese el valor del 2do Diámetro.")
+                d_2=float(input())
+                calculo=((d_1*d_2)/2)
+                print ("El área es " + str(calculo) +".")
+            
+            if entrada == ("E"):
+                print("Ingrese el valor del Radio")
+                radio=float(input())
+                calculo=math.pi*(radio)**2
+                print ("El área es " + str(calculo) +".")
+    else:
+        print("Figura no contemplada.")
     
 proceso (entrada)
